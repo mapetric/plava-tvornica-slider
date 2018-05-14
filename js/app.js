122a4c494bd7e1060b17466437778d5729463beb
Marko Petric <markopetric.fer@gmail.com>
2018-05-14 23:45:56 +0200

    added overflow button

--- js/app.js
+++ js/app.js
@@ -12,8 +12,20 @@ const shiftRight = () => {
     });
 }
 
+toggleOwerflow = () => {
+    // toggle overflow for all the lists
+    $('.slider-list').each((i, slider) => {
+        if ($(slider).css("overflow") === 'visible') {
+            $(slider).css({"overflow" : "hidden"});
+        } else {
+            $(slider).css({"overflow" : "visible"});
+        }
+    });
+}
+
 const init = () => {
-    // add event listeners for navigation buttons
+    // add event listeners for navigation and overflow buttons
+    $('.owerflow-button').click(toggleOwerflow);
     $('.push-right').click(shiftRight);
     $('.push-left').click(shiftLeft);
 }
