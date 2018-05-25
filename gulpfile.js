e6f4b897a964ec8b8f0a495384d860518b955090
Marko Petric <markopetric.fer@gmail.com>
2018-05-25 23:45:08 +0200

    set up Gulp

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const babel = require('gulp-babel');
+const livereload = require('gulp-livereload');
+
+gulp.task('sass-compile', function() {
+    return gulp.src('./app/scss/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('./app/css'))
+        .pipe(livereload());
+});
+
+gulp.task('js-compile', function() {
+    return gulp.src('./app/ES6/*.js')
+        .pipe(babel({
+            presets: ['env']
+        }))
+        .pipe(gulp.dest('./app/js'))
+        .pipe(livereload());
+});
+
+gulp.task('watch', function() {
+    livereload.listen();
+    gulp.watch('./app/scss/*.scss', ['sass-compile']);
+    gulp.watch('./app/ES6/*.js', ['js-compile']);
+});
+
+gulp.task('serve', ['sass-compile', 'js-compile', 'watch']);
